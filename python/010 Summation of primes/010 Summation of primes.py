efd44f58a6afa4cde18be45659c66e085cefca70
Joshua Gutz <jgutz87@gmail.com>
2023-01-19 03:12:39 -0600

    Updated 010 Summation of primes.py

--- python/010 Summation of primes/010 Summation of primes.py
+++ python/010 Summation of primes/010 Summation of primes.py
@@ -5,11 +5,11 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-###################################################################################
-############### NOTE THIS VERSION GETS THE WRONG ANSWER 27931306768 ###############
-###################################################################################
+####################################################################################
+############### NOTE THIS VERSION GETS THE RIGHT ANSWER 142913828922 ###############
+####################################################################################
 
-# The problem with the code is that it's only counting prime numbers in the range of 0 to limit, but the requirement was to find the sum of all the primes below two million.
+# The problem with the code is that it is checking the bit value of the prime numbers in the array, but it should be checking the bit value of the composites. In other words, if the bit value is 1, it means the number is composite and should be excluded from the sum, whereas if the bit value is 0, it means the number is prime and should be included in the sum.
 
 # Here is the corrected version of the code:
 
@@ -18,15 +18,15 @@ import array
 def sum_of_primes_below_two_million():
     limit = 2000000
     primes = array.array('B', [1] * (limit // 8 + 1))
-    primes[0] = primes[1] = 0
+    primes[0] = primes[1] = 1
 
     for i in range(2, int(limit ** 0.5) + 1):
-        if primes[i // 8] & (1 << (i % 8)):
+        if not primes[i // 8] & (1 << (i % 8)):
             for j in range(i * i, limit, i):
-                primes[j // 8] &= ~(1 << (j % 8))
+                primes[j // 8] |= (1 << (j % 8))
 
-    return sum(i for i in range(2, limit) if primes[i // 8] & (1 << (i % 8)))
+    return sum(i for i in range(2, limit) if not primes[i // 8] & (1 << (i % 8)))
 
 print(sum_of_primes_below_two_million())
 
-# In this corrected version, The range of the iteration starts from 2 instead of 0 and it ends at limit. Also, the sum that is returned is the sum of the prime numbers between 2 and limit instead of the sum of all numbers between 0 and limit.
+# This is the correct version of the code, It uses a bit array to store the composite numbers, which greatly reduces the memory usage of the algorithm and makes it more efficient. The iteration starts from 2 till the square root of limit and for each prime number, it marks all the multiples of that number as composite numbers. Finally, it returns the sum of all prime numbers that are not marked as composite numbers.
