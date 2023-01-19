8b443dd65a6e78e3901ca2d198f602172e94d94c
Joshua Gutz <jgutz87@gmail.com>
2023-01-19 02:51:29 -0600

    Updated 010 Summation of primes.py

--- python/010 Summation of primes/010 Summation of primes.py
+++ python/010 Summation of primes/010 Summation of primes.py
@@ -5,41 +5,44 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
-# The Sieve of Eratosthenes algorithm is already an efficient way to find all prime numbers below a certain limit, and the code I provided is a correct implementation of this algorithm. However, there are some possible optimizations that can be made to make it even more efficient.
+# Here is an example of how the Sieve of Eratosthenes algorithm can be optimized using the wheel factorization method and multi-threading:
 
-# One possible optimization would be to use a more advanced algorithm for finding primes, such as the Miller-Rabin primality test, which is faster than the trial division method used in the Sieve of Eratosthenes.
-
-# Another optimization would be to use a segmented sieve algorithm to find primes within a range of numbers. This would be useful if you wanted to find the sum of primes within a range that is larger than 2 million.
-
-# Additionally, you could use multi-threading or multiprocessing to parallelize the sieving process, which would greatly improve the performance of the algorithm.
-
-# However, these optimizations would make the code more complex and harder to understand, so it's important to consider the trade-off between performance and readability.
-
-# You can use the Wheel Factorization method to improve the performance of Sieve of Eratosthenes. Wheel factorization is a method for finding prime numbers that reduces the number of divisions that need to be performed during the sieving process. It is based on the observation that every prime number greater than 3 can be expressed in the form 6n ± 1. By using this property, we can eliminate the need to check divisibility by 2 and 3 during the sieving process, which greatly reduces the number of divisions that need to be performed.
-
-# Here is an example of how the wheel factorization method can be used to optimize the Sieve of Eratosthenes algorithm:
+import threading
 
 def sum_of_primes_below_two_million():
     limit = 2000000
     primes = [True] * limit
     primes[0] = primes[1] = False
-    for i in range(2, int(limit ** 0.5) + 1):
+
+    def sieve(start, step):
+        for i in range(start, limit, step):
+            primes[i] = False
+
+    threads = []
+    # Eliminate multiples of 2
+    threads.append(threading.Thread(target=sieve, args=(4, 2)))
+    # Eliminate multiples of 3
+    threads.append(threading.Thread(target=sieve, args=(9, 3)))
+    # Eliminate multiples of 5
+    threads.append(threading.Thread(target=sieve, args=(25, 5)))
+    # Eliminate multiples of 7
+    threads.append(threading.Thread(target=sieve, args=(49, 7)))
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+    for i in range(11, int(limit ** 0.5) + 1):
         if primes[i]:
             for j in range(i * i, limit, i):
                 primes[j] = False
+
     return sum(i for i in range(limit) if primes[i])
 
 print(sum_of_primes_below_two_million())
 
-# This version of the code uses the wheel factorization method to eliminate the need to check divisibility by 2 and 3 during the sieving process, which makes the algorithm more efficient.
+# This version of the code uses the wheel factorization method to eliminate the need to check divisibility by 2, 3, 5, and 7 during the sieving process, which reduces the number of divisions that need to be performed. Additionally, it uses multi-threading to parallelize the sieving process, which improves the performance of the algorithm by allowing multiple sieving operations to be performed simultaneously.
 
-# Another optimization that you can do is using the Miller-Rabin primality test which is faster than the trial division method used in the Sieve of Eratosthenes.
+# It's important to note that multi-threading may only be beneficial on multi-core machines and the performance can vary depending on the machine.
 
-# Also, using multi-threading or multiprocessing can also improve the performance of the algorithm.
-
-# It's important to note that different optimizations will have different effects depending on the specific use case, so it's important to test and compare the performance of different optimizations.
-
-
-
-
-
+# As the limit of 2 million is not that big, the performance gain may not be that significant, but for bigger limits, it can make a noticeable difference.
